91a7fbadbbb5e482a0598a56a007c1aef42b80b1
Michael Lei <michaelleie0ca@gmail.com>
2023-06-04 11:44:06 -0700

    Add dark mode toggle and edit light mode bg color

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,9 +1,18 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
   darkMode: "class",
-  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  content: [
+    "./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}",
+    "node_modules/preline/dist/*.js",
+  ],
+
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["Inter", "sans-serif"],
+      },
+    },
   },
-  plugins: [],
+  plugins: [require("preline/plugin")],
 };
